dbaa8c06f122fb061caa2bf4d11a71b79924b366
Vidhya Balaji <vidhya.balaji12@gmail.com>
2022-01-10 21:07:32 +0530

    some chNGES

--- libraryservice_2/src/main/java/com/vblearning/libraryservice/libraryservice/RentBookRepositary.java
+++
@@ -1,7 +0,0 @@
-package com.vblearning.libraryservice.libraryservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RentBookRepositary extends JpaRepository<RentBook, Integer> {
-
-}
--- libraryservice_2/src/main/java/com/vblearning/libraryservice/libraryservice/UserRepositary.java
+++
@@ -1,7 +0,0 @@
-package com.vblearning.libraryservice.libraryservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepositary extends JpaRepository<User, Integer> {
-
-}
